997689a84cbcb16ef5e2810f0101ff177b80a98c
bryce1999 <44781640+bryce1999@users.noreply.github.com>
2020-04-15 22:50:22 -0500

    Create chuck_norris_joke.py
    Created a copy in the root repository to satisfy criteria, " I should be able to clone your repository, pip install -r requirements.txt, python {your_script_name}.py and see a Chuck Norris joke printed to the console."

---
+++ chuck_norris_joke.py
@@ -0,0 +1,29 @@
+# Native imports
+import sys
+
+# Third party imports
+import requests
+
+def request_joke() -> str:
+    """Return a chuck norris joke as a string
+    Requests a joke from api.chucknorris.io.
+    """
+    response = None
+
+    try:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(e)
+
+    response.raise_for_status()
+
+    # Get the response json as a python Dict
+    json_data = response.json()
+
+    return json_data['value']
+
+
+if __name__ == '__main__':
+    print(f'{request_joke()}')
+    sys.exit()
