81f570faf5701a18fc9695f0ab6d0d5925707478
Bryce Breedlove <brycebreedlo@gmail.com>
2020-04-15 22:13:13 -0500

    Add request_joke method to retrieve a chuck norris joke

---
+++ Challenge_0416/chuck_norris_joke.py
@@ -0,0 +1,12 @@
+# Native imports
+import os
+import sys
+
+# Third party imports
+import requests
+
+def request_joke() -> str:
+    """Return a chuck norris joke as a string
+
+    Requests a joke from 
+    """
