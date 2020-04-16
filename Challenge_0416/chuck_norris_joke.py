cc26e6849e9de6e9fbe60b25563b14ebc3d9fa79
Bryce Breedlove <brycebreedlo@gmail.com>
2020-04-15 22:17:58 -0500

    finish get_joke method

--- Challenge_0416/chuck_norris_joke.py
+++ Challenge_0416/chuck_norris_joke.py
@@ -8,5 +8,21 @@ import requests
 def request_joke() -> str:
     """Return a chuck norris joke as a string
 
-    Requests a joke from 
+    Requests a joke from api.chucknorris.io.
     """
+    response = None
+
+    try:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(e)
+
+    response.raise_for_status()
+
+    # Get the response json as a python Dict
+    json_data = response.json()
+
+    return json_data['value']
+
+
